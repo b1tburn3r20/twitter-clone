ebe28a7e683929f5ef0291bd520ebaa22fdf99b4
b1tburn3r20 <alejandrojosuebermudez@gmail.com>
2023-05-16 11:53:02 -0500

    right before it breaks with oauth

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const postCtrl = require('../controllers/posts');
 
 // GET /movies
 router.get('/', postCtrl.show)
+
+//GOOGLE OAUTH
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+    // Which passport strategy is being used?
+    'google',
+    {
+        // Requesting the user's profile and email
+        scope: ['profile', 'email'],
+        // Optionally force pick account every time
+        // prompt: "select_account"
+    }
+));
+//
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+    'google',
+    {
+        successRedirect: '/movies',
+        failureRedirect: '/movies'
+    }
+));
+// OAuth logout route
+router.get('/logout', function (req, res) {
+    req.logout(function () {
+        res.redirect('/movies');
+    });
+});
+
 router.post('/new', postCtrl.create)
 
 //
